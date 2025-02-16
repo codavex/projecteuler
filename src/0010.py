360384d5c5c9130bdcaa4eb5e6f15d12736f4439
David Cox <dave@codavex.co.uk>
2025-02-16 13:49:52 +0000

    initial solution

---
+++ src/0010.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import getopt
+import sympy
+import sys
+
+
+def main(argv):
+    limit = None
+    helpString = f"{sys.argv[0]} -l <limit>"
+
+    try:
+        opts, args = getopt.getopt(argv, "l:", ["limit="])
+    except getopt.GetoptError:
+        print(helpString)
+        sys.exit()
+
+    # sort out options
+    try:
+        for opt, arg in opts:
+            if opt in ("-l", "--limit"):
+                limit = int(arg)
+        if limit is None:
+            raise ValueError("Limit not set")
+    except ValueError:
+        print(helpString)
+        sys.exit()
+
+    primes = sympy.primerange(0, limit)
+    sum = 0
+    for i in primes:
+        sum += i
+
+    print(sum)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
